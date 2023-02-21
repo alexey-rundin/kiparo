d0e51c5f38a0b1b5dd4439e7ca4e3041549330b8
Alexey Rundin <a.rundin@rtcomm.ru>
2023-02-21 13:25:23 +0700

    small fix

--- src/main/java/com/kiparo/homework/Main.java
+++ src/main/java/com/kiparo/homework/Main.java
@@ -8,6 +8,7 @@ import com.kiparo.homework.service.DownloadService;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -22,11 +23,52 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Нажмите 1 что-бы скачать JSON, 2 - XML: ");
-        downloader(scanner);
+        try {
+            System.out.print("Нажмите 1 что-бы скачать JSON, 2 - XML, 0 - Выход: ");
+            downloader(scanner);
+            System.out.print("1 - вывести все новости, 2 - поиск по keyword. Введите 1 или 2: ");
+            printerWithFinder(scanner);
+        } catch (ExecutionException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
 
-        System.out.print("1 - вывести все новости, 2 - поиск по keyword. Введите 1 или 2: ");
-        printerWithFinder(scanner);
+    private static void downloader(Scanner scanner) throws ExecutionException {
+        while (true) {
+            int selectedFileType = scanner.nextInt();
+            if (selectedFileType == 1) {
+                System.out.println("Выбрано скачивание JSON");
+                try {
+                    System.out.println("Скачиваем JSON");
+                    downloadService.downloadFile(DataType.getFileUrl(DataType.JSON), DataType.getFileName(DataType.JSON));
+
+                    System.out.println("Обрабатываем полученные данные");
+                    dataService.parseAndSaveJSONData();
+                    System.out.println("Полученные данные успешно сохранены.");
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (selectedFileType == 2) {
+                System.out.println("Выбрано скачивание XML");
+                try {
+                    System.out.println("Скачиваем XML");
+                    downloadService.downloadFile(DataType.getFileUrl(DataType.XML), DataType.getFileName(DataType.XML));
+
+                    System.out.println("Обрабатываем полученные данные");
+                    dataService.parseAndSaveXMLData();
+                    System.out.println("Полученные данные успешно сохранены.");
+                    break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (selectedFileType == 0) {
+                throw new ExecutionException(new Throwable("Exit selected"));
+            } else {
+                System.out.println("Введите 1 или 2 для выбора типа файла. 0 для выхода.");
+                System.out.print("Нажмите 1 что-бы скачать JSON, 2 - XML, 0 - Выход: ");
+            }
+        }
     }
 
     private static void printerWithFinder(Scanner scanner) {
@@ -42,42 +84,6 @@ public class Main {
                 System.out.print("Введите ключевое слово для поиска: ");
                 String keyword = scanner.next();
                 dataService.findAndPrintNewsByKeyword(keyword);
-                break;
-            }
-            default:
-                System.out.println("Введите 1 или 2.");
-        }
-    }
-
-    private static void downloader(Scanner scanner) {
-        int selectedFileType = scanner.nextInt();
-        switch (selectedFileType) {
-            case 1: {
-                System.out.println("Выбрано скачивание JSON");
-                try {
-                    System.out.println("Скачиваем JSON");
-                    downloadService.downloadFile(DataType.getFileUrl(DataType.JSON), DataType.getFileName(DataType.JSON));
-
-                    System.out.println("Обрабатываем полученные данные");
-                    dataService.parseAndSaveJSONData();
-                    System.out.println("Полученные данные успешно сохранены.");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-            case 2: {
-                System.out.println("Выбрано скачивание XML");
-                try {
-                    System.out.println("Скачиваем XML");
-                    downloadService.downloadFile(DataType.getFileUrl(DataType.XML), DataType.getFileName(DataType.XML));
-
-                    System.out.println("Обрабатываем полученные данные");
-                    dataService.parseAndSaveXMLData();
-                    System.out.println("Полученные данные успешно сохранены.");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
                 break;
             }
             default:
